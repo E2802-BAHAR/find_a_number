840f485f0c770ed2702ad17a45d37c4825f8e69b
Bahar <floresjune132@gmail.com>
2020-09-22 02:48:19 +0300

    upload files

---
+++ app.js
@@ -0,0 +1,58 @@
+const y = Math.floor(Math.random() * 100 + 1);
+guessnumber = 1;
+
+document.getElementById("btn").addEventListener("click",
+
+    function playgame() {
+        var audio1 = document.getElementById("music1");
+        audio1.play();
+
+        const x = document.getElementById("userguess").value;
+        if (isNaN(x)) {
+            document.getElementById("output").innerHTML = "Enter a valid number";
+        } else {
+
+            if (x > y) {
+                document.getElementById("output").innerHTML = "I am thinking about lower number";
+                guessnumber += 1;
+                document.getElementById("numberofguess").innerHTML = guessnumber;
+            } else if (x < y) {
+                document.getElementById("output").innerHTML = "I am thinking about higher number";
+                guessnumber += 1;
+                document.getElementById("numberofguess").innerHTML = guessnumber;
+            } else {
+                document.getElementById("output").innerHTML = "You are the winner";
+                document.getElementById("numberofguess").innerHTML = guessnumber + " times";
+                audio1.pause();
+                var audio2 = document.getElementById("music2");
+                audio2.play();
+                var z = document.getElementById('celebreting');
+                z.style.visibility = 'visible';
+                var a = document.getElementById('rstbtn');
+                a.style.visibility = 'visible';
+
+
+
+
+
+
+            }
+        }
+    });
+
+document.getElementById("rstbtn").addEventListener("click",
+    function stopgame() {
+        var audio2 = document.getElementById("music1");
+        audio2.pause();
+        var z = document.getElementById('celebreting');
+        z.style.visibility = 'hidden';
+        var a = document.getElementById('rstbtn');
+        a.style.visibility = 'hidden';
+        document.getElementById("output").innerHTML = "You stopped game";
+        document.getElementById("text_number").innerHTML = "If you want to play again enter a number";
+
+<<<<<<< HEAD
+    });
+=======
+    });
+>>>>>>> fdc6af3e557459c6e6de70e35972cfb71c8a5f9c
